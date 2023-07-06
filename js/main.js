fa880aff2c0cba8dac30bb842b7605f0cf8e69a2
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-06 16:10:10 -0500

    Done with project, finalize CSS last details

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
 "use strict"
 console.log(`hello from main.js`);
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+// local storage =================================
+(() => {
+    let local = localStorage.getItem("latestCoffee");
+    let obj = JSON.parse(local);
+    if(obj){
+        coffees.push(obj);
+
+    }
+})()
+// ================================================
+
 let coffeeCards = document.querySelector('#coffees');
 const searchBar = document.getElementById('search');
 let roastSelection = document.querySelector('#roast-selection');
 const submitCoffee = document.querySelector('#addSubmit');
 const addBar = document.getElementById('addCoffee');
 let addRoast = document.querySelector('#roast-addition');
+
+
 
 function renderCoffee(coffee) {
     let roastImage;
@@ -21,11 +51,11 @@ function renderCoffee(coffee) {
     let html = `
         <div class="coffee-card">
         <div class="row cesar justify-center">
-            <h2>${coffee.name}</h2>
+            <h2 class="coffee-name">${coffee.name}</h2>
             <p class="coffee-paragraph">${coffee.roast}</p>
         </div>
             <img class="image-timer" src="${roastImage}" alt="coffee">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta doloremque esse veritatis.</p>
+            <p class="coffee-paragraph-detail">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta doloremque esse veritatis.</p>
         </div>
     `;
     return html;
@@ -67,23 +97,7 @@ function updateCoffees(e) {
 
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+
 
 coffeeCards.innerHTML = renderCoffees(coffees);
 
@@ -96,6 +110,13 @@ function addCoffee (e) {
     e.preventDefault();
     let newCoffee = {id: coffees.length + 1, name: addBar.value, roast: addRoast.value};
     coffees.push(newCoffee);
+
+    // local storage =================================
+    const coffeePlus = JSON.stringify(newCoffee);
+    localStorage.setItem("latestCoffee", coffeePlus);
+    // ================================================
+
     coffeeCards.innerHTML = renderCoffees(coffees);
+
 }
 submitCoffee.addEventListener('click', addCoffee);
